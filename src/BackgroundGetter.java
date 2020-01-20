4fa587ac00b8ce73cdeec8dc76ec3f24aa2e4b42
[Jae Park] <[jaepark052@gmail.com]>
2020-01-20 10:23:54 -0800

    Add methods for fetching & copying files from the system

--- src/BackgroundGetter.java
+++ src/BackgroundGetter.java
@@ -1,12 +1,38 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+
 
 public class BackgroundGetter {
 
-    public static void main(String[] args) {
+    public static File[] files;
+    public static File[] suitFiles;
+    public static int currentFileNum;
+    public static int suit;
+
+    public static void main(String[] args) throws IOException {
 
         new File("C:\\Users\\jaepa\\Desktop\\Background_Images").mkdirs(); // creating a folder to store images
 
+       // fetchFiles();
+       // copyFiles();
+        getNumber();
+
+    }
+
+    public static void getNumber(){
+
+        File[] files = new File("C:\\Users\\jaepa\\Desktop\\Background_Images").listFiles();
+        currentFileNum = files.length + 1;
+        //System.out.println(currentFileNum);
+
+    }
+
+    public static void createFile(){
         try {
             File myObj = new File("C:\\Users\\jaepa\\Desktop\\Background_Images\\testA.txt");
             if (myObj.createNewFile()) {
@@ -18,5 +44,29 @@ public class BackgroundGetter {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static void fetchFiles() throws NullPointerException, IOException {
+        files = new File("C:\\Users\\jaepa\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets").listFiles();
+
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isFile()) {
+                    if (files[i].length() >= 340000) {
+                        copyFiles(files[i]);
+                        //System.out.println(files[i].length());
+
+                    }
+                }
+        }
+
+    }
+
+    public static void copyFiles(File file) throws IOException {
+//        Path origin = Paths.get("C:\\Users\\jaepa\\Desktop\\folderA");
+//        Path des = Paths.get("C:\\Users\\jaepa\\Desktop\\FolderB");
+
+        File source = new File(file.getCanonicalPath());
+        File dest = new File("C:\\Users\\jaepa\\Desktop\\FolderB\\" + currentFileNum + ".jpg");
+        Files.copy(source.toPath(), dest.toPath());
     }
     }
